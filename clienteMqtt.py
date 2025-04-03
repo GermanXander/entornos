d43f6cfe842bdb63e7f5c62df3da4d8e74483819
Germán Xander <german.xander@fio.unam.edu.ar>
2025-04-03 11:22:12 -0300

    2025

--- clienteMqtt.py
+++ clienteMqtt.py
@@ -1,12 +1,12 @@
 import asyncio, ssl, certifi
-from asyncio_mqtt import Client, ProtocolVersion
+from aiomqtt import Client, ProtocolVersion
 from environs import Env
 
 env = Env()
 env.read_env() #lee el archivo con las variables. por defecto .env
 
 async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    tls_context = ssl.create_default_context(purpose = ssl.Purpose.SERVER_AUTH)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
@@ -17,10 +17,9 @@ async def main():
         port=8883,
         tls_context=tls_context,
     ) as client:
-        async with client.messages() as messages:
-            await client.subscribe("#")
-            async for message in messages:
-                print(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        await client.subscribe("#")
+        async for message in client.messages:
+            print(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
